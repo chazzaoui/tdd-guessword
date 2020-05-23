15443cab7c2b32bc82d727902f7bd195318b3c4e
Charaf Azzaoui <charafazzaoui@gmail.com>
2020-05-24 01:29:44 +0200

    (feat): add lettermatch  helper function

---
+++ src/helpers/index.js
@@ -0,0 +1,6 @@
+export function getLetterMatchCount(guessedWord, secretWord) {
+  const secretLetterSet = new Set(secretWord.split(""));
+  const guessedWordSet = new Set(guessedWord.split(""));
+
+  return [...secretLetterSet].filter((letter) => guessedWordSet.has(letter)).length
+}
