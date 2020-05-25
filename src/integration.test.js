c24275c1740c1134017777d32eed269745ceb378
Charaf Azzaoui <charafazzaoui@gmail.com>
2020-05-25 18:23:00 +0200

    (feat): add first few parts of guessword tests

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,9 +2,26 @@ import { storeFactory } from './utils/testUtil';
 import {guessword} from './actions'
 
 describe('Guess word action dispatcher', () => {
+    const secretWord = 'party';
+    const faultyGuess = 'train';
     describe('No guessed words', () => {
+        let store;
+        const initialState = {secretWord};
+        beforeEach( ()=> {
+            store = storeFactory(initialState);
+        })
         test('should update state correctly for unsuccesfull guess', () => {
-            
+            store.dispatch(guessword(faultyGuess));
+            const newState = store.getState();
+            const expectedState = {
+                ...initialState,
+                success: false,
+                guessedWords:[{
+                    guessword : faultyGuess,
+                    letterMatchCount : 3
+                }]
+            }
+            expect(newState).toEqual(expectedState);
         });
         test('should update state correctly for succesfull guess', () => {
             
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 
 import success from './succesReducer';
 import guessedWords from './guessedWordsReducer';
+import secretWord from './secretWordReducer'
 
 export default combineReducers({
     success,
-    guessedWords
+    guessedWords,
+    secretWord
 }) 
